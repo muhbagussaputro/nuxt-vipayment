d545bacba0caa711f7f1b12ba8aa5b5c1bc05e87
muhbagussaputro <muhbagussaputro.id@gmail.com>
2025-07-21 00:58:20 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
   // Runtime config
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE
     }
   },
   
